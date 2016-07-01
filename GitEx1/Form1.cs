5c8765f08d765023f7c4efbcf22e8569dac3cb9c
Jinwon <jinwon7@naver.com>
2016-07-01 16:09:35 +0900

    Text 표시

--- GitEx1/Form1.cs
+++ GitEx1/Form1.cs
@@ -16,5 +16,10 @@ namespace GitEx1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Hello, World!";
+        }
     }
 }
